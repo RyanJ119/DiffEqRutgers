76c852288dde274c2d9e86b14eca033425924ee1
RyanJ119 <61523480+RyanJ119@users.noreply.github.com>
2021-06-30 13:03:52 -0400

    Add files via upload

--- NewSIRwithMarkovNoODE45.m
+++ NewSIRwithMarkovNoODE45.m
@@ -1,16 +1,19 @@
 % Model parameters
 
-% to test for a good choice of beta and gamma, N*beta/gamma should be about
+% to test for a good choice of beta and gamma, beta/gamma should be about
 % 1-5
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Define Markov Chain for Infected
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Interactions
 Tr = [0.9 0.05 0.02 0.03; % Mutations probability for infected1
-     0.02 0.9 0.035 0.0;% Mutations probability for infected2
-     0.02 0.03 0.95 0.0;% Mutations probability for infected3
-     0.02 0.03 0.05 0.9]; % Mutations probability for infected
- %Transition Matrix
+     0.02 0.9 0.03 0.05;% Mutations probability for infected2
+     0.02 0.03 0.9 0.05;% Mutations probability for infected3
+     0.02 0.03 0.05 0.9]; % Mutations probability for infected4
+ 
+ 
+ %Transition Matrix Print
+ %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 figure;
 
 imagesc(Tr);
@@ -22,22 +25,23 @@ h.XTick = 1:4;
 h.YTick = 1:4;
 title 'Transition Matrix Heatmap';
 hold on;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 delta = 0.0; % rate of immunity loss
 N = 60000000; % Total population N = S + I + R
 
 
-I10 = 1; % initial number of infected
+I10 = 100; % initial number of infected
 I20 = 1; % initial number of infected
 I30 = 1; % initial number of infected
-I40 = 100; % initial number of infected
+I40 = 1; % initial number of infected
 
 
 T = 180; % period of 180 days
 dt = 1; % time interval for updating
 
 % Calculate the model
-[S,I1, I2, I3, I4,R] = sir_model(delta,N,I10, I20, I30, I40,T,dt);
+[S,I1, I2, I3, I4,R, Tr] = sir_model(delta,N,I10, I20, I30, I40,Tr, T,dt);
 % Plots that display the epidemic outbreak
 %fprintf('Value of parameter R0 is %.2f',N*beta/gamma)
 t = 0:dt:T-dt;
@@ -49,7 +53,7 @@ xlabel('Days'); ylabel('Number of individuals');
 legend('S','I1','I2','I3','I4','R');
 hold on;
 % Map
-function [S,I1, I2, I3, I4,R, T] = sir_model(delta,N,I10,I20,I30,I40,T,dt)
+function [S,I1, I2, I3, I4,R,Tr, T] = sir_model(delta,N,I10,I20,I30,I40,Tr, T,dt)
     % if delta = 0 we assume a model without immunity loss
     
     
@@ -65,19 +69,14 @@ function [S,I1, I2, I3, I4,R, T] = sir_model(delta,N,I10,I20,I30,I40,T,dt)
     I4 = zeros(1,T/dt);
     I4(1) = I40;
     R = zeros(1,T/dt);
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Define Markov Chain for Infected
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Interactions
-Tr = [0.9 0.05 0.02 0.03; % Mutations probability for infected1
-     0.02 0.9 0.035 0.0;% Mutations probability for infected2
-     0.02 0.03 0.9 0.05;% Mutations probability for infected3
-     0.02 0.03 0.05 0.9]; % Mutations probability for infected
- %Transition Matrix
-    
-    
+
     for t = 1:(T/dt)-1
         
+% to test for a good choice of beta and gamma, beta/gamma should be about
+% 1-5
+%0.3+0.5*sin(t)
+%
 
-        
         beta1 = .3; 
         beta2 = .3; 
         beta3 = .3; %% Infection Rates (Beta(I_i))
@@ -86,14 +85,14 @@ Tr = [0.9 0.05 0.02 0.03; % Mutations probability for infected1
         gamma1 = 0.12;
         gamma2 = 0.12; 
         gamma3 = 0.12;  %% Recovery Rate (Gamma(I_i))
-        gamma4 = 0.002; 
+        gamma4 = 0.12; 
         
         gamma = [gamma1 gamma2 gamma3 gamma4];  %vectorize recovery rates
         beta = [beta1 beta2 beta3 beta4];%vectorize infection rates
         I = [I1(t) I2(t) I3(t) I4(t)];
 
         if t~=0
-            if mod(t,20) == 0
+            if mod(t,50) == 0
                 
                 I = ((I/sum(I))*Tr)*sum(I); %find probability distribution, multiply by transition matrix,then multiply by total infected again 
                 I1(t) = I(1);
