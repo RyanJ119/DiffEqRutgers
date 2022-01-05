b91604906917ead85ca2176e36579dc09f2441e4
RyanJ119 <61523480+RyanJ119@users.noreply.github.com>
2022-01-05 12:08:15 -0500

    Add files via upload
    Updated model with the MDE method of updating mutations

--- MDEBaseCode.m
+++ MDEBaseCode.m
@@ -3,16 +3,18 @@ clear all;
 %parameters: vas (number of variants acting on the system), n (mutations of variants), R0(replications rate), Beta(infection
 %rate), Gamma(recovery rate), markov chain (mutation factor),  
 
-vas = 5; %number of variants affecting the system: variants associated with specific data used to build R function
-n = 1000; %number of different mutations of variants n>1 (use multiples of vas)
+vas = 4; %number of variants affecting the system: variants associated with specific data used to build R function
+n = 100; %number of different mutations of variants n>1 (use multiples of vas)
 mu = linspace(0,1,n) ; %x range of distributions: taking 0-1 and placing n evenly distributed points 
 initI = zeros(1, n); %initialize the infected populations with zeroes
-
-initI(n/2) = 1; %set middle element to one infected
-%initI(1) = 1; 
- for i = 1:vas
- initI((i/vas)*n) = 1;     %update the initial infected populations using values or functions
- end
+for i = 1:n
+    
+initI(i) = 1; %set middle element to one infected
+end
+%initI(n/4) = 1; 
+%  for i = 1:vas
+%  initI((i/vas)*n) = 1;     %update the initial infected populations using values or functions
+%  end
 
 %initI = variants/100+1;% this will need to be fixed to be a function matching the four variants
 
@@ -45,7 +47,7 @@ Iend=[];
 daysUpdate = 2; % Number of days between mutations (swapping between infected groups) 
 totalDays = 500; %total days of program
 i = totalDays/daysUpdate;  % number of times to run the solver
-
+probdist = zeros(i+1,n);
 
  %%%%%%%%%%%%%%%%%% Call the solver i times with a transition of infected inbetween calls
  for c = 1:i
@@ -83,69 +85,88 @@ i = totalDays/daysUpdate;  % number of times to run the solver
     R = vertcat(R, YSol(:,n+2));
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Update step
 m = 20;
-delv = 1/10;
+v = .5*m;
+delv = 1/v;
 delm = 1/m;
 
 flip = I';              %Turn I in order to do mutation step
 Iend = flip(:, end);
-probdist = Iend'./sum(Iend);
-F = zeros(1, n);
-F2 = [];
-v = [];
- for k = 1:n 
+
+probdist(c,:) = Iend'./sum(Iend);
+F = zeros(n);
+
+for k = 1:n
+        F(k) = sum(probdist(c,1:k));
         
-        if probdist(1,k) > 0
-            F(k) = sum(probdist(1:k));                     %should F definitely be cumulative distribution including zeros?
-        end                                     % Should F be calculated fully before the update sequence, or during?
-    end
-    for k = 1:n
         
-        if  F(k) > 0
+        
+        for j = 1:ceil(probdist(c,k)/delm)
             
-            for j = 1:floor(probdist(k)/delm)         %Does not happen when I(k) < .05
-                %              if k == 1
-                %                 F2(k,j) = 0;
-                %              end
+            if probdist(c, k) >=delm
                 
-                if j == floor(probdist(k)/delm) && k>1 && j>1
-                    F2(k,j) = F(k-1) + F(k) - F2(k, j-1);
-                end
-                if  k>1
-                    F2(k,j) = F(k-1)+delm*F(k);
-                end
                 
-                if k > 1
-                    
-                    v(k,j) = floor((1/delv)*rho(F(k-1) + sum(F2(k,1:j))));
-                    e1 = zeros(1, n);
-                    e2 = zeros(1, n);
-                    e1(k) = 1;
-                    if probdist(k) -  e1*F2(k,j) >=0
-                        
-                        if (k+v(k,j))<=n && (k+v(k,j)>=1)
-                            e2(k+v(k,j)) = 1;
-                            probdist = probdist -  e1*F2(k,j) + e2*F2(k,j);
-                            %             else
-                            %                 w = 5
-                            
-                        elseif (k+v(k,j))>n
-                            e2(n) = 1;
-                            probdist = probdist -  e1*F2(k,j) + e2*F2(k,j);
-                        elseif (k+v(k,j))<1
-                            e2(1) = 1;
-                            probdist = probdist -  e1*F2(k,j) + e2*F2(k,j);
-                        end
-                    end
+                %pieces = zeros(1,j);
+                % F2 = zeros(1,j);
+                if      j ~= ceil(probdist(c, k)/delm) && j>1
+                    F2(j) = F2(j-1)+delm;
+                    pieces(j) = delm;
+                elseif      j == 1
+                    F2(j) = delm;
+                    pieces(j) = delm;
+                elseif j == ceil(probdist(c, k)/delm)
+                    F2(j) = probdist(c, k);
+                    pieces(j) =probdist(c, k)-F2(j-1) ;
                 end
             end
+            if probdist(c, k) <=delm
+                F2(j) = probdist(c, k);
+                pieces(j) = probdist(c, k);
+            end
+     
+
+            %vel = zeros(1,j);
+            if k > 1
+                vel(j) = floor((1/delv)*phi(F(k-1)+F2(j))+0.000001);
+
+            elseif k == 1
+                vel(j) = 0;
+            end
+            
+            e1 = zeros(1, n);
+            e2 = zeros(1, n);
+            e1(k) = 1;
+            
+            if (k+vel(j))<=n && (k+vel(j)>=1)
+                
+                e2(k+vel(j)) = 1;
+                
+                %  e2
+            elseif (k+vel(j))>n
+                e2(n) = 1;
+                
+            elseif (k+vel(j))<1
+                e2(1) = 1;
+                
+            end
+            probdist(c+1,:) =  probdist(c+1,:)+e2*pieces(j);
+            
+            %         if  e2 ~=zeros(1,n)
+            %             e2
+            %         end
+        end
+end
+   
+    for w = 1:i+1
+        for q = 1:n
+        if probdist(i+1, n) < .00005
+            probdist(i+1, n) = 0;
+        end
         end
     end
-
-    
-    
+   
     
 
-Iend = probdist.*sum(Iend);
+Iend = probdist(c+1,:).*sum(Iend);
 
         
 
@@ -211,6 +232,7 @@ for j = 1:vas
 end
 
 figure;
+
 h(1) = plot(tSol,S, 'DisplayName', 'susceptible');
 hold on;
 for i = 1:vas
@@ -225,6 +247,20 @@ legend(h, 'FontSize', 18)
 xlabel("Days", 'FontSize', 18) 
 ylabel("Number of Individuals", 'FontSize', 18)
 h = [];
+
+
+figure;
+
+subplot(2,2,1);
+plot(mu,gamma)
+title('Gamma')
+subplot(2,2,2); 
+plot(mu,Ro)
+title('Replication Rate')
+subplot(2,2,3); 
+plot(mu,beta)
+title('Beta')
+
 function dYdt = SIRmodels(t,Y,n, beta, gamma)
    % lOI = .02
     S = Y(1);   %% Susceptibles
@@ -239,7 +275,7 @@ function dYdt = SIRmodels(t,Y,n, beta, gamma)
     dYdt = [dSdt ;  dIdt; dRdt];% Solution matrix 
 end
 
-function [result] = rho(x)
+function [result] = phi(x)
 
 result = 2*x-1;
 
